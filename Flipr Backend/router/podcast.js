0452f75ac64df487d05f83f5b9aa3f6126e0bf87
Om Prakash <ompra.rox@gmail.com>
2023-04-21 23:23:58 +0530

    Podcast Search api added

--- Flipr Backend/router/podcast.js
+++ Flipr Backend/router/podcast.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const { getAllPodcasts, createPodcast, getPodcastById, updatePodcast, deletePodcast } = require("../controllers/podcast");
 const router = express();
 
 router.get('/get-allpodcasts',getAllPodcasts);
+
+router.post('/create-podcast', createPodcast);
+
+router.get('/get-podcast/:id', getPodcastById);
+
+router.put('/update-podcast/:id', updatePodcast);
+
+router.delete('/delete-podcast/:id', deletePodcast);
 
 
 
