c89978bafd1a11e4c32976a5486e074deff2f734
Om Prakash <ompra.rox@gmail.com>
2023-04-23 01:28:05 +0530

    Netlify setup in frontend done

--- fliprfrontend/src/pages/_app.js
+++ fliprfrontend/src/pages/_app.js
@@ -1,22 +1,21 @@
-import '@/styles/globals.css'
-import '@/styles/Navbar.scss'
-import '@/styles/podcast.scss'
-import Store from '@/utils/Redux/Store';
+import "@/styles/globals.css";
+import "@/styles/Navbar.scss";
+import "@/styles/podcast.scss";
+import Store from "@/utils/Redux/Store";
 import { ThemeProvider } from "@material-tailwind/react";
-import { Provider } from 'react-redux';
-import 'react-toastify/dist/ReactToastify.css';
-import "slick-carousel/slick/slick.css"; 
+import { Provider } from "react-redux";
+import "react-toastify/dist/ReactToastify.css";
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { ToastContainer } from "react-toastify";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider>
-      <Provider store={Store}>
+    <Provider store={Store}>
+      <ThemeProvider>
         <Component {...pageProps} />
         <ToastContainer />
-      </Provider>
-    </ThemeProvider>
-  )
-
+      </ThemeProvider>
+    </Provider>
+  );
 }
