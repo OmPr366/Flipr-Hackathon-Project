3ab4ec09d81610cca986bf933f53a5d085442016
ashish <ashishmohite16052@gmail.com>
2023-04-22 21:44:37 +0530

    seperate page for vid

--- fliprfrontend/src/components/Cards/PodcastCard.js
+++ fliprfrontend/src/components/Cards/PodcastCard.js
@@ -8,45 +8,55 @@ import Link from "next/link";
 import React from "react";
 import { useDispatch } from "react-redux";
 import { setPodcast } from "@/utils/Redux/PodcastSlice";
+import { useRouter } from 'next/navigation';
 
-const PodcastCard = ({ podcast ,isAdmin }) => {
+const PodcastCard = ({ podcast, isAdmin }) => {
   const dispatch = useDispatch()
+  const { push } = useRouter();
   // console.log(props.podcast);
 
   const openPodcast = () => {
     dispatch(setPodcast(podcast))
   }
 
+  const redirectPodcast = ()=>{
+    push('/listen');
+  }
+
   return (
-   
-      <div className="bg-black shadow-lg rounded-lg overflow-hidden p-4 w-40 h-60  cursor-pointer border  podcastCard  ">
-        <div className="bg-white w-6 h-6 rounded-full absolute justify-center items-center flex  mt-1 shadow-lg playIcon">
-          {podcast?.type === "audio" ? (
-            <MusicalNoteIcon color="black" width={20} />
-          ) : (
-            <VideoCameraIcon color="black" width={20} />
-          )}
-          {/* <MusicalNoteIcon color="black" width={20} /> */}
-          {/* <VideoCameraIcon color="black" width={20} /> */}
-        </div>
-        <div className="relative pb-32  rounded-md overflow-hidden  ">
-          <Image
-            className="absolute inset-0 h-full w-full object-cover ImageBox"
-            src={podcast?.image}
-            alt={podcast?.title}
-            width={600}
-            height={600}
-          />
-        </div>
-        {/* Play button icon */}
-        <button className=" bg-green-600 w-10 h-10 rounded-full flex justify-center items-center absolute postCastPlayBtn ">
+
+    <div className="bg-black shadow-lg rounded-lg overflow-hidden p-4 w-40 h-60  cursor-pointer border  podcastCard  ">
+      <div className="bg-white w-6 h-6 rounded-full absolute justify-center items-center flex  mt-1 shadow-lg playIcon">
+        {podcast?.type === "audio" ? (
+          <MusicalNoteIcon color="black" width={20} />
+        ) : (
+          <VideoCameraIcon color="black" width={20} />
+        )}
+        {/* <MusicalNoteIcon color="black" width={20} /> */}
+        {/* <VideoCameraIcon color="black" width={20} /> */}
+      </div>
+      <div className="relative pb-32  rounded-md overflow-hidden  ">
+        <Image
+          className="absolute inset-0 h-full w-full object-cover ImageBox"
+          src={podcast?.image}
+          alt={podcast?.title}
+          width={600}
+          height={600}
+        />
+      </div>
+      {/* Play button icon */}
+      <button className=" bg-green-600 w-10 h-10 rounded-full flex justify-center items-center absolute postCastPlayBtn ">
+        {podcast?.type === "audio" ? (
           <PlayIcon color="white" width={30} onClick={openPodcast} />
-        </button>
+        ) : (
+          <PlayIcon color="white" width={30} onClick={redirectPodcast} />
+        )}
+      </button>
 
-        {/* Icon */}
+      {/* Icon */}
 
-        {isAdmin ? <Link href={`/dashboard/podcast/${podcast?._id}`}>
-          <div>
+      {isAdmin ? <Link href={`/dashboard/podcast/${podcast?._id}`}>
+        <div>
           <div className="text-md font-bold mb-1 mt-2 text-white">
             {podcast?.title}
           </div>
@@ -55,8 +65,8 @@ const PodcastCard = ({ podcast ,isAdmin }) => {
               ? podcast?.description.slice(0, 42) + "..."
               : podcast?.description}
           </div>
-          </div>
-          </Link>:
+        </div>
+      </Link> :
 
         <div>
           <div className="text-md font-bold mb-1 mt-2 text-white">
@@ -67,8 +77,8 @@ const PodcastCard = ({ podcast ,isAdmin }) => {
               ? podcast?.description.slice(0, 42) + "..."
               : podcast?.description}
           </div>
-        </div> }
-      </div>
+        </div>}
+    </div>
   );
 };
 
