a2db6c5d65e65190bd1eafb7661d32f78d764afb
Om Prakash <ompra.rox@gmail.com>
2023-04-23 01:32:46 +0530

    Image unoptimized added

--- fliprfrontend/next.config.js
+++ fliprfrontend/next.config.js
@@ -2,7 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['picsum.photos','res.cloudinary.com']
+    domains: ['picsum.photos','res.cloudinary.com'],
+    unoptimized : true
   }
 }
 
